8b9988944e408a3cfe01ba8e37b51ead2e300f7f
kevin88duong <kevin88.duong@gmail.com>
2023-12-23 03:15:33 +0000

    url

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 async function getData(){
-  const res = await fetch("http://localhost:3000/api/posts", { next: { revalidate: 20 } })
+  const res = await fetch("https://kdgallery.vercel.app/api/posts", { next: { revalidate: 20 } })
 
   if(!res.ok){
     throw new Error("Failed")
